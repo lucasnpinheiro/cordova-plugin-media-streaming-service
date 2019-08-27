838e69b3e1d5681d519519ba3c2a389d4789af2d
Paul Kibe Njuguna <wacujuguna@gmail.com>
2019-08-27 07:53:15 -0300

    Fix methods signatures

--- www/mediastreaming.js
+++ www/mediastreaming.js
@@ -1,16 +1,16 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-  start: function(channelId, channelName, notificationId, mediaStreams, selectedIndex) {
-    exec(null, null, "MediaStreamingPlugin", "start", [channelId || "playback_channel", channelName || "Playback", notificationId || "1", JSON.stringify(mediaStreams || []), selectedIndex || "0"]);
+  start: function(options, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "MediaStreamingPlugin", "start", [options.channelId || "playback_channel", options.channelName || "Playback", options.notificationId || "1", JSON.stringify(options.mediaStreams || []), options.selectedIndex || "0"]);
   },
-  play: function(selectedIndex) {
-    exec(null, null, "MediaStreamingPlugin", "play", [selectedIndex || "0"]);
+  play: function(selectedIndex, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "MediaStreamingPlugin", "play", [selectedIndex || "0"]);
   },
-  pause: function() {
-    exec(null, null, "MediaStreamingPlugin", "pause", []);
+  pause: function(successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "MediaStreamingPlugin", "pause", []);
   },
-  stop: function() {
-    exec(null, null, "MediaStreamingPlugin", "stop", []);
+  stop: function(successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "MediaStreamingPlugin", "stop", []);
   }
 };
